0eb843142755294931e5cdf64dc8b91e0deaecb9
99061142 <xarbprive@gmail.com>
2023-02-07 21:04:49 +0100

    Used anchor element from react-bootstrap
    Used anchor element from react-bootstrap instead of the standard html5 anchor element.

--- src/about.jsx
+++ src/about.jsx
@@ -1,18 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { Anchor } from 'react-bootstrap';
 
 export default function About() {
     return (
         <div className="px-3 pt-3 text-white" id="about" style={{paddingBottom:"400px"}}>
             <h1>Xander Briem | <span>Software developer</span></h1>
-            
-            <a className="text-white" href="https://www.linkedin.com/in/xander-briem/" target="_blank">
+
+            <Anchor className="text-white" href="https://www.linkedin.com/in/xander-briem/" target="_blank">
                 <FontAwesomeIcon className="fa-brands fa-linkedin fa-xl" style={{padding:'6px'}} icon={faLinkedin} />
-            </a>
-            <a className="text-white" href="https://github.com/99061142" target="_blank">
+            </Anchor>
+            <Anchor className="text-white" href="https://github.com/99061142" target="_blank">
                 <FontAwesomeIcon className="fa-brands fa-linkedin fa-xl" style={{padding:'6px'}} icon={faGithub} />
-            </a>
+            </Anchor>
 
             <p className="mx-3" style={{width:"60%"}}>Ik hou me bezig met de front en backend kant van websites en games.</p>
         </div>
