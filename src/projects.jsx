16278c271262193825625cf666c99c5fec23ead7
99061142 <xarbprive@gmail.com>
2023-02-07 20:38:18 +0100

    Deleted unnecessary div container

--- src/projects.jsx
+++ src/projects.jsx
@@ -4,17 +4,15 @@ import Project from './project'
 export default function Projects() {
     return (
         <div id="projects" className="p-3">
-            <div>
-                <h2 className="text-left">Projecten</h2>
-                <div className="d-flex flex-wrap justify-content-center">
-                    {featuredProjects
-                    .filter(dict => dict.github_url || dict.page_url)
-                    .map((project) => {
-                        return (
-                            <Project project={project} key={project.id} id={project.id} />
-                        )
-                    })}
-                </div>
+            <h2 className="text-left">Projecten</h2>
+            <div className="d-flex flex-wrap justify-content-center">
+                {featuredProjects
+                .filter(dict => dict.github_url || dict.page_url)
+                .map((project) => {
+                    return (
+                        <Project project={project} key={project.id} id={project.id} />
+                    )
+                })}
             </div>
         </div>
     );
