0701fb06d8b6eb53419d37f21aaf5c36e55b30b4
99061142 <xarbprive@gmail.com>
2023-02-07 16:07:26 +0100

    Used react bootstrap to create anchor links

--- src/project.jsx
+++ src/project.jsx
@@ -1,20 +1,16 @@
+import { Container, Anchor } from 'react-bootstrap';
+
 const Project = ({project, id}) => {
     return (
         <div className="p-2 m-3 project" key={id}>
             <p className="title text-primary">{project.title}</p>
             <p className="languages">{project.languages.join(', ')}</p>
             <p className="description">{project.description.length != 0 ? project.description : 'No description found.'}</p>
-            
-            <div className="container">
-                <div className="row">
-                    <a className="btn col-12 col-xl-6 my-2" href={project.page_url} target="_blank">
-                        Live Demo {">"}
-                    </a>
-                    <a className="btn col-12 col-xl-6 my-2" href={project.github_url} target="_blank">
-                        GitHub {">"}
-                    </a>
-                </div>
-            </div>
+
+            <Container>
+                <Anchor className="btn col-12 col-xl-6 my-2" href={project.page_url}>Live Demo {">"}</Anchor>
+                <Anchor className="btn col-12 col-xl-6 my-2" href={project.github_url}>GitHub {">"}</Anchor>
+            </Container>
         </div>
     );
 }
