af496a9160c12ada931fc8bf34594ea1f03d3531
99061142 <xarbprive@gmail.com>
2023-02-07 20:35:59 +0100

    Deleted unnecessary elements / used react elements
    * Deleted unnecessary elements.
    * Used react-bootstrap elements.
    * Added / deleted styling inside the element(s) class names.

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,48 +1,49 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { Container, Anchor, Row, Col } from 'react-bootstrap';
 
-export default function Footer() {
+function Footer() {
     const TODAY = new Date()
     const YEAR = TODAY.getFullYear();
 
     return (
-        <footer className="mx-auto w-100 w-100 bg-dark pt-4">     
-            <div className="container">
-                <div className="row justify-content-end">
-                    <div className="col-12 col-md-6 col-lg-3 mb-3">
-                        <p className="m-0 mb-2 text-left">Socials</p>
-                        <div className="d-flex justify-content-start">
-                            <a className="mx-2" href="https://www.linkedin.com/in/xander-briem/" target="_blank">
-                                <FontAwesomeIcon className="fa-brands fa-linkedin fa-lg border rounded-circle" style={{padding:'6px'}} icon={faLinkedin} />
-                            </a>
-                            <a className="mx-2" href="https://github.com/99061142" target="_blank">
-                                <FontAwesomeIcon className="fa-brands fa-linkedin fa-lg border rounded-circle" style={{padding:'6px'}} icon={faGithub} />
-                            </a>
-                        </div>
-                    </div>
+        <footer className="mx-auto w-100 bg-dark pt-4">     
+            <Container>
+                <Row className="justify-content-end">
+                    <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                        <p className="mb-2">Socials</p>
+                        <Anchor className="mx-2" href="https://www.linkedin.com/in/xander-briem/" target="_blank">
+                            <FontAwesomeIcon className="fa-lg border rounded-circle" style={{padding:'6px'}} icon={faLinkedin} />
+                        </Anchor>
+                        <Anchor className="mx-2" href="https://github.com/99061142" target="_blank">
+                            <FontAwesomeIcon className="fa-lg border rounded-circle" style={{padding:'6px'}} icon={faGithub} />
+                        </Anchor>
+                    </Col>
 
-                    <div className="col-12 col-md-6 col-lg-3 mb-3">
-                        <p className="m-0 mb-2">Contact</p>
-                        <div className="mx-2">      
-                            <strong className="text-muted">E </strong>
-                            <a className="text-decoration-none" href="mailto:xanderbwerk@gmail.com">xanderbwerk@gmail.com</a>
-                        </div>
-                    </div>
+                    <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                        <p className="mb-2">Contact</p>
+                        <strong className="d-inline-block ms-2 me-1 text-muted">E</strong>
+                        <Anchor className="text-decoration-none" href="mailto:xanderbwerk@gmail.com">xanderbwerk@gmail.com</Anchor>
+                    </Col>
+                </Row>
+            </Container>
+
+            <div className="flex-column text-center">
+                <div>
+                    <Anchor className="text-decoration-none arrow d-inline-flex align-items-center" href="/">
+                            <span></span>
+                            <span></span>
+                    </Anchor>
+                </div>
+                <div>
+                    <Anchor className="text-decoration-none" href="/">BACK TO TOP</Anchor>
                 </div>
             </div>
 
-            <a className="text-decoration-none arrow" href="/">
-                <span></span>
-                <span></span>
-            </a>
-            <div className="text-center mb-3">
-                <a className="text-decoration-none" href="/">BACK TO TOP</a>
-            </div>
-
-            <div className="mx-5 border-top border-2 py-2">
-                <p className="m-0">@ {YEAR} Xander Briem</p>
-            </div>
+            <p className="mx-5 border-top border-2 py-2 m-0">@ {YEAR} Xander Briem</p>
         </footer>
     );
 }
+
+export default Footer
