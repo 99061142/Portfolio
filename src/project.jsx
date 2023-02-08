32ee5a6d408a9ecf7fb49a2a1e3e892c7368363e
99061142 <xarbprive@gmail.com>
2023-02-08 17:33:47 +0100

    Used correct hrefs for github/demo

--- src/project.jsx
+++ src/project.jsx
@@ -1,6 +1,6 @@
 import { Button, Container } from 'react-bootstrap';
 
-const Project = ({project, id}) => {
+const Project = ({ project, id }) => {
     return (
         <div className="p-2 m-3 project text-black" key={id}>
             <p className="title text-primary">{project.title}</p>
@@ -8,10 +8,10 @@ const Project = ({project, id}) => {
             <p className="description">{project.description.length != 0 ? project.description : 'No description found.'}</p>
 
             <Container>
-                <Button className="text-dark col-12 col-xl-6 my-2" variant={project.page_url ? "primary" : "danger"} href={project.page_url} target="_blank">Live Demo {">"}</Button>
-                <Button className="text-dark col-12 col-xl-6 my-2" variant={project.github_url ? "primary" : "danger"} href={project.github_url} target="_blank">GitHub {">"}</Button>
-            </Container>
-        </div>
+                <Button className="text-dark col-12 col-xl-6 my-2" variant={project.page_url ? "primary" : "danger"} href={'/' + project.page_url} target="_blank">Live Demo {">"}</Button >
+                <Button className="text-dark col-12 col-xl-6 my-2" variant={project.github_url ? "primary" : "danger"} href={'/' + project.github_url} target="_blank">GitHub {">"}</Button >
+            </Container >
+        </div >
     );
 }
 export default Project;
