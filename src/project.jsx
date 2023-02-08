9e380943741e829d25581995375e47ed01f76e2f
99061142 <xarbprive@gmail.com>
2023-02-08 18:19:03 +0100

    check description length with !== instead of !=

--- src/project.jsx
+++ src/project.jsx
@@ -5,7 +5,7 @@ const Project = ({ project, id }) => {
         <div className="p-2 m-3 project text-black" key={id}>
             <p className="title text-primary">{project.title}</p>
             <p className="languages">{project.languages.join(', ')}</p>
-            <p className="description">{project.description.length != 0 ? project.description : 'No description found.'}</p>
+            <p className="description">{project.description.length !== 0 ? project.description : 'No description found.'}</p>
 
             <Container>
                 <Button className="text-dark col-12 col-xl-6 my-2" variant={project.page_url ? "primary" : "danger"} href={'/' + project.page_url} target="_blank">Live Demo {">"}</Button >
