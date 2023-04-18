589a60ff7c2f0176a8cfac5b3ebe0d5a151b33fe
99061142 <xarbprive@gmail.com>
2023-04-18 14:50:05 +0200

    Added frameworks /  made it more readable

--- src/project.jsx
+++ src/project.jsx
@@ -1,16 +1,52 @@
-import { Button, Container } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
-const Project = ({ project }) => {
+function Project({ title, languages, description, page_url, github_url, frameworks }) {
     return (
-        <div className="p-2 m-3 project text-black">
-            <p className="title text-primary">{project.title}</p>
-            <p className="languages">{project.languages.join(', ')}</p>
-            <p className="description">{project.description.length !== 0 ? project.description : 'No description found.'}</p>
-
+        <div className="p-2 m-3 project text-black" >
+            <p className="text-primary">
+                {title}
+            </p>
+            {frameworks &&
+                <p className={languages ? 'mb-2' : null}>
+                    {frameworks.join(', ')}
+                </p>
+            }
+            {languages &&
+                <p>
+                    {languages.join(', ')}
+                </p>
+            }
+            <p style={{ color: description ? null : 'red' }}>
+                {description || 'No information found.'}
+            </p>
             <Container>
-                <Button className="text-dark col-12 col-xl-6 my-2" variant={project.page_url ? "primary" : "danger"} href={'/' + project.page_url} disabled={!project.page_url} target="_blank">Live Demo {">"}</Button >
-                <Button className="text-dark col-12 col-xl-6 my-2" variant={project.github_url ? "primary" : "danger"} href={project.github_url} disabled={!project.github_url} target="_blank">GitHub {">"}</Button >
-            </Container >
+                <Row>
+                    <Col xs={12} xl={6}>
+                        <Button
+                            className="text-dark my-2 w-100"
+                            variant={page_url ? 'primary' : 'danger'}
+                            href={page_url}
+                            disabled={!page_url}
+                            style={{ cursor: page_url ? null : 'not-allowed' }}
+                            target="_blank"
+                        >
+                            Live Demo {">"}
+                        </Button>
+                    </Col>
+                    <Col xs={12} xl={6}>
+                        <Button
+                            className="text-dark my-2 w-100"
+                            variant={github_url ? 'primary' : 'danger'}
+                            href={github_url}
+                            disabled={!github_url}
+                            style={{ cursor: github_url ? null : 'not-allowed' }}
+                            target="_blank"
+                        >
+                            GitHub {">"}
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
         </div >
     );
 }
