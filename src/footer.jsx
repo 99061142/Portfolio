9012e00f45e87abd51e9912ce33f9f8c2e4860a4
99061142 <xarbprive@gmail.com>
2023-04-18 14:49:10 +0200

    Changed class to function

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,16 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { Container, Anchor, Row, Col, Button } from 'react-bootstrap';
-import { Component } from 'react';
 
-class Footer extends Component {
-    constructor() {
-        super();
-        const TODAY = new Date()
-        this.year = TODAY.getFullYear();
-    }
+function Footer() {
+    const TODAY = new Date()
+    const YEAR = TODAY.getFullYear();
 
-    scrollToTop() {
+    const scrollToTop = () => {
         window.scrollTo({
             top: 0,
             left: 0,
@@ -18,57 +14,52 @@ class Footer extends Component {
         });
     }
 
-    render() {
-        return (
-            <footer className="bg-dark pt-4 px-3" >
-                <Container>
-                    <Row className="justify-content-end">
-                        <Col className="col-12 col-md-6 col-lg-3 mb-3">
-                            <p className="mb-2">Socials</p>
-                            <Anchor className="mx-2" href="https://www.linkedin.com/in/xander-briem/" target="_blank">
-                                <FontAwesomeIcon className="fa-lg border rounded-circle" style={{ padding: '6px' }} icon={faLinkedin} />
-                            </Anchor>
-                            <Anchor className="mx-2" href="https://github.com/99061142" target="_blank">
-                                <FontAwesomeIcon className="fa-lg border rounded-circle" style={{ padding: '6px' }} icon={faGithub} />
-                            </Anchor>
-                        </Col>
-
-                        <Col className="col-12 col-md-6 col-lg-3 mb-3">
-                            <p className="mb-2">Contact</p>
-                            <strong className="d-inline-block ms-2 me-1 text-muted">E</strong>
-                            <Anchor className="text-decoration-none" href="mailto:xanderbwerk@gmail.com">xanderbwerk@gmail.com</Anchor>
-                        </Col>
-                    </Row>
-                </Container>
-
-                <div className="flex-column text-center">
-                    <div>
-                        <Button
-                            variant="text"
-                            className="text-decoration-none arrow d-inline-flex align-items-center"
-                            onClick={() => this.scrollToTop()}
-                        >
-                            <span></span>
-                            <span></span>
-                        </Button>
-                    </div>
-                    <div>
-                        <Button
-                            variant="text"
-                            className="text-decoration-none text-white"
-                            onClick={() => this.scrollToTop()}
-                        >
-                            BACK TO TOP
-                        </Button>
-                    </div>
+    return (
+        <footer className="bg-dark pt-4 px-3" >
+            <Container>
+                <Row className="justify-content-end">
+                    <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                        <p className="mb-2">Socials</p>
+                        <Anchor className="mx-2" href="https://www.linkedin.com/in/xander-briem/" target="_blank">
+                            <FontAwesomeIcon className="fa-lg border rounded-circle" style={{ padding: '6px' }} icon={faLinkedin} />
+                        </Anchor>
+                        <Anchor className="mx-2" href="https://github.com/99061142" target="_blank">
+                            <FontAwesomeIcon className="fa-lg border rounded-circle" style={{ padding: '6px' }} icon={faGithub} />
+                        </Anchor>
+                    </Col>
+                    <Col className="col-12 col-md-6 col-lg-3 mb-3">
+                        <p className="mb-2">Contact</p>
+                        <strong className="d-inline-block ms-2 me-1 text-muted">E</strong>
+                        <Anchor className="text-decoration-none" href="mailto:xanderbwerk@gmail.com">xanderbwerk@gmail.com</Anchor>
+                    </Col>
+                </Row>
+            </Container>
+            <div className="flex-column text-center">
+                <div>
+                    <Button
+                        variant="text"
+                        className="text-decoration-none arrow d-inline-flex align-items-center"
+                        onClick={() => scrollToTop()}
+                    >
+                        <span></span>
+                        <span></span>
+                    </Button>
                 </div>
-
-                <div className="mx-4 mt-3 border-top border-2">
-                    <p className="m-2">@ {this.year} Xander Briem</p>
+                <div>
+                    <Button
+                        variant="text"
+                        className="text-decoration-none text-white"
+                        onClick={() => scrollToTop()}
+                    >
+                        BACK TO TOP
+                    </Button>
                 </div>
-            </footer >
-        );
-    }
+            </div>
+            <div className="mx-4 mt-3 border-top border-2">
+                <p className="m-2">@ {YEAR} Xander Briem</p>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer
