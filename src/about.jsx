83348689f08b29b7279c7de6abc92f48e5c2b6c1
99061142 <xarbprive@gmail.com>
2023-05-06 22:01:32 +0200

    Added separate socials component

--- src/about.jsx
+++ src/about.jsx
@@ -1,20 +1,15 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { Anchor } from 'react-bootstrap';
+import Socials from './socials';
 
 function About() {
     return (
-        <div className="px-3 pt-3 pb-5 text-black" id="about">
-            <h1>Xander Briem | <span>Software developer</span></h1>
-
-            <Anchor className="text-black" href="https://www.linkedin.com/in/xander-briem/" target="_blank">
-                <FontAwesomeIcon className="fa-brands fa-linkedin fa-xl" style={{ padding: '6px' }} icon={faLinkedin} />
-            </Anchor>
-            <Anchor className="text-black" href="https://github.com/99061142" target="_blank">
-                <FontAwesomeIcon className="fa-brands fa-linkedin fa-xl" style={{ padding: '6px' }} icon={faGithub} />
-            </Anchor>
-
-            <p className="mx-3" style={{ width: "60%" }}>Ik hou me bezig met de front en backend kant van websites en games.</p>
+        <div className='ps-3 pt-3 pb-5 text-black' id='about'>
+            <h1>
+                Xander Briem | <span>Software developer</span>
+            </h1>
+            <Socials anchorClassNames='text-black' iconClassNames='fa-xl' />
+            <p className='mx-3' style={{ width: '60%' }}>
+                Ik hou me bezig met de front en backend kant van websites en games.
+            </p>
         </div>
     );
 }
