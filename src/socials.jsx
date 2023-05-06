ae24bf66426f0cc7760513f8213a8fd031fbcb04
99061142 <xarbprive@gmail.com>
2023-05-06 22:10:45 +0200

    Return all socials for the footer component

---
+++ src/socials.jsx
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { Anchor } from 'react-bootstrap';
+
+function Socials({ anchorClassNames = '', iconClassNames = '' }) {
+    const SOCIALS = [
+        {
+            'href': 'https://www.linkedin.com/in/xander-briem/',
+            'icon': faLinkedin
+        },
+        {
+            'href': 'https://github.com/99061142',
+            'icon': faGithub,
+        }
+    ];
+    return (
+        SOCIALS.map(({ href, icon }, key) =>
+            <Anchor
+                className={anchorClassNames}
+                href={href}
+                target='_blank'
+                key={key}
+            >
+                <FontAwesomeIcon
+                    className={iconClassNames}
+                    style={{ padding: '6px' }}
+                    icon={icon}
+                />
+            </Anchor>
+        )
+    )
+}
+
+export default Socials
