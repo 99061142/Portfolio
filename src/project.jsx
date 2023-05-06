f773acbe09d1cfed7b190e6ab34819c13834eb33
99061142 <xarbprive@gmail.com>
2023-05-06 22:07:32 +0200

    Call separate component to create the buttons

--- src/project.jsx
+++ src/project.jsx
@@ -1,53 +1,20 @@
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import ProjectButtons from './projectButtons';
 
-function Project({ title, languages, description, page_url, github_url, frameworks }) {
+function Project(project) {
     return (
-        <div className="p-2 m-3 project text-black" >
-            <p className="text-primary">
-                {title}
+        <div className='p-2 m-3 project text-black'>
+            <p className='text-primary'>
+                {project.title}
             </p>
-            {frameworks &&
-                <p className={languages ? 'mb-2' : null}>
-                    {frameworks.join(', ')}
-                </p>
-            }
-            {languages &&
-                <p>
-                    {languages.join(', ')}
-                </p>
-            }
-            <p style={{ color: description ? null : 'red' }}>
-                {description || 'No information found.'}
+            <p>
+                {project.skills.join(', ')}
             </p>
-            <Container>
-                <Row>
-                    <Col xs={12} xl={6}>
-                        <Button
-                            className="text-dark my-2 w-100"
-                            variant={page_url ? 'primary' : 'danger'}
-                            href={page_url}
-                            disabled={!page_url}
-                            style={{ cursor: page_url ? null : 'not-allowed' }}
-                            target="_blank"
-                        >
-                            Live Demo {">"}
-                        </Button>
-                    </Col>
-                    <Col xs={12} xl={6}>
-                        <Button
-                            className="text-dark my-2 w-100"
-                            variant={github_url ? 'primary' : 'danger'}
-                            href={github_url}
-                            disabled={!github_url}
-                            style={{ cursor: github_url ? null : 'not-allowed' }}
-                            target="_blank"
-                        >
-                            GitHub {">"}
-                        </Button>
-                    </Col>
-                </Row>
-            </Container>
-        </div >
+            <p style={{ color: project.description ? null : 'red' }}>
+                {project.description || 'No description found.'}
+            </p>
+            <ProjectButtons {...project} />
+        </div>
     );
 }
+
 export default Project;
