8497a94b078b583fd2a9c31d2e9e3c9b30c684e1
99061142 <xarbprive@gmail.com>
2023-05-06 22:10:17 +0200

    Made skills dict correct

--- src/skills.jsx
+++ src/skills.jsx
@@ -4,23 +4,42 @@ import { faReact } from '@fortawesome/free-brands-svg-icons';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function Skills() {
-    const SKILLS = {
-        'React': faReact,
-        'JS': faSquareJs,
-        'Python': faPython,
-        'HTML': faHtml5,
-        'CSS': faCss3Alt
-    };
+    const SKILLS = [
+        {
+            skill: 'React',
+            icon: faReact
+        },
+        {
+            skill: 'JS',
+            icon: faSquareJs
+        },
+        {
+            skill: 'Python',
+            icon: faPython
+        },
+        {
+            skill: 'HTML',
+            icon: faHtml5
+        },
+        {
+            skill: 'CSS',
+            icon: faCss3Alt
+        }
+    ];
     return (
-        <div id='skills' className='px-3 py-5 text-black'>
-            <h2>SKILLS</h2>
-            <p className='mx-3'>De skills die ik binnen mijn projecten gebruik</p>
+        <div id='skills' className='ps-3 py-5 text-black'>
+            <h2>
+                SKILLS
+            </h2>
+            <p className='ms-3'>
+                De skills die ik binnen mijn projecten gebruik
+            </p>
             <Container>
                 <Row className='justify-content-around text-center'>
-                    {Object.entries(SKILLS).map(([skill, icon], key) =>
+                    {SKILLS.map(({ skill, icon }, key) =>
                         <Col xs={4} md={3} lg={true} key={key}>
                             <FontAwesomeIcon size='4x' icon={icon} />
-                            <p>{skill}</p>
+                            <p className='mb-0'>{skill}</p>
                         </Col>
                     )}
                 </Row>
