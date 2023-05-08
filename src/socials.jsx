5c13e7ce39e05f99f737f96f72688930b68fadc0
99061142 <xarbprive@gmail.com>
2023-05-09 00:43:33 +0200

    Made code cleaner (no things added)

--- src/socials.jsx
+++ src/socials.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { Anchor } from 'react-bootstrap';
 
-function Socials({ anchorClassNames = '', iconClassNames = '' }) {
+function Socials({ anchorClassNames, iconClassNames }) {
     const SOCIALS = [
         {
             'href': 'https://www.linkedin.com/in/xander-briem/',
@@ -16,19 +16,19 @@ function Socials({ anchorClassNames = '', iconClassNames = '' }) {
     return (
         SOCIALS.map(({ href, icon }, key) =>
             <Anchor
-                className={anchorClassNames}
+                className={anchorClassNames || null}
                 href={href}
                 target='_blank'
                 key={key}
             >
                 <FontAwesomeIcon
-                    className={iconClassNames}
+                    className={iconClassNames || null}
                     style={{ padding: '6px' }}
                     icon={icon}
                 />
             </Anchor>
         )
-    )
+    );
 }
 
 export default Socials
