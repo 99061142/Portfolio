ee3b1083c9a9c30be56b63cd2c760666ccab2125
99061142 <xarbprive@gmail.com>
2023-05-09 00:38:54 +0200

    Added css span element inside span itself
    Deleted styling inside the css file, and added it to the element itself.

--- src/about.jsx
+++ src/about.jsx
@@ -2,15 +2,28 @@ import Socials from './socials';
 
 function About() {
     return (
-        <div className='ps-3 pt-3 pb-5 text-black' id='about'>
+        <div
+            id='about'
+            className='px-3 pt-3 pb-1 text-black'
+        >
             <h1>
-                Xander Briem | <span>Software developer</span>
+                Xander Briem | <span
+                    style={{ background: 'linear-gradient(rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 60%, rgba(255, 255, 0, 1) 50%, rgba(255, 255, 0, 1) 30%)' }}
+                >
+                    Software developer
+                </span>
             </h1>
-            <Socials anchorClassNames='text-black' iconClassNames='fa-xl' />
-            <p className='mx-3' style={{ width: '60%' }}>
+            <Socials
+                anchorClassNames='text-black'
+                iconClassNames='fa-xl'
+            />
+            <p
+                className='mx-3'
+                style={{ width: '60%' }}
+            >
                 Ik hou me bezig met de front en backend kant van websites en games.
             </p>
-        </div>
+        </div >
     );
 }
 
