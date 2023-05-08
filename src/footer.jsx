90b9d0cb6cd08a095ac6128b7577819edd338bb8
99061142 <xarbprive@gmail.com>
2023-05-09 00:47:00 +0200

    Added separate components / used correct elements
    * Called 'Socials' component to show social info.
    * Called 'Contacts' component to show contact info.
    * Used 'Button' element to create the arrow / 'back to top' text to let the user go back to the top.
    * Made code cleaner.

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,58 +1,84 @@
-import { Container, Anchor, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Socials from './socials';
 import Contacts from './contacts';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCopyright, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-scroll';
 
 function Footer() {
-    const NOW = new Date()
+    const NOW = new Date();
     const YEAR = NOW.getFullYear();
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
-
     return (
-        <footer className='bg-dark pt-4 px-3'>
+        <footer className='bg-dark pt-4 px-3 text-white'>
             <Container>
                 <Row className='justify-content-end'>
-                    <Col xs={12} md={6} lg={3} className='mb-3'>
-                        <p className='mb-2'>Socials</p>
-                        <Socials anchorClassNames='mx-2' iconClassNames='fa-lg border rounded-circle' />
+                    <Col
+                        xs={12}
+                        md={6}
+                        lg={3}
+                        className='mb-3'
+                    >
+                        <p className='mb-2'>
+                            Socials
+                        </p>
+                        <Socials
+                            anchorClassNames='mx-2'
+                            iconClassNames='fa-lg border rounded-circle text-white'
+                        />
                     </Col>
-                    <Col xs={12} md={6} lg={3} className='mb-3'>
-                        <p className='mb-2'>Contact</p>
-                        <Contacts />
+                    <Col
+                        xs={12}
+                        md={6}
+                        lg={3}
+                        className='mb-3'
+                    >
+                        <p className='mb-2'>
+                            Contact
+                        </p>
+                        <Contacts
+                            anchorClassNames='text-decoration-none text-white'
+                            iconClassNames='pe-2 text-secondary'
+                        />
                     </Col>
                 </Row>
             </Container>
-            <div className='flex-column text-center'>
-                <div>
-                    <Button
-                        variant='text'
-                        className='text-decoration-none arrow d-inline-flex align-items-center'
-                        onClick={scrollToTop}
+            <ul
+                className='text-center ps-0'
+                style={{ listStyleType: 'none' }}
+            >
+                <li className='my-2'>
+                    <Link
+                        to='root'
+                        role='button'
                     >
-                        <span></span>
-                        <span></span>
-                    </Button>
-                </div>
-                <div>
-                    <Button
-                        variant='text'
-                        className='text-decoration-none text-white'
-                        onClick={scrollToTop}
+                        <FontAwesomeIcon
+                            size='2xl'
+                            icon={faChevronUp}
+                            bounce
+                        />
+                    </Link>
+                </li>
+                <li >
+                    <Link
+                        to='root'
+                        role='button'
                     >
                         BACK TO TOP
-                    </Button>
+                    </Link>
+                </li>
+            </ul>
+            <div className='mx-4 mt-3 border-top border-2'>
+                <div className='py-2 d-flex align-items-center'>
+                    <FontAwesomeIcon
+                        className='text-white mx-2'
+                        icon={faCopyright}
+                    />
+                    <p className='mb-0'>
+                        {YEAR} Xander Briem
+                    </p>
                 </div>
             </div>
-            <div className='mx-4 mt-3 border-top border-2'>
-                <p className='m-2'>@ {YEAR} Xander Briem</p>
-            </div>
-        </footer>
+        </footer >
     );
 }
 
